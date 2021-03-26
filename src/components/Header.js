96eea2da2cdf619fa01818cd37beec07f5d2a8db
Madrakhimov Donyor@DESKTOP-19A59FQ <madaminovdoniyor@gmail.com>
2021-03-27 00:42:15 +0900

    file

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return(
+        <header className="header">
+            <img src="https://www.logoarena.com/contestimages/public_new/1862/4796_1359032747_kanaltechnic16.png"
+                 alt=""/>
+        </header >
+    )
+}
+export default Header;
---
+++ src/components/Profile.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Profile = () => {
+    return(
+        <div className="content">
+            <div>
+                <img src="https://d1whtlypfis84e.cloudfront.net/guides/wp-content/uploads/2019/07/23090714/nature-1024x682.jpeg" alt=""/>
+            </div>
+            <div>
+                avatar
+            </div>
+            <div>
+                My post
+                <div>News post</div>
+                <div>
+                    psot 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Profile;
